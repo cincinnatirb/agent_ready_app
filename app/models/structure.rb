c3d37ddbc913fe31146ff141d2c2861e9a3a1215
Tim Mecklem <timothy@mecklem.com>
2025-03-20 11:41:17 -0400

    Add validation back

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -10,6 +10,6 @@ class Structure < ApplicationRecord
     other: "other"
   }
 
-  # validates :building_type, :length, :width, presence: true
-  # validates :length, :width, numericality: { greater_than: 0 }, allow_nil: true
+  validates :building_type, :length, :width, presence: true
+  validates :length, :width, numericality: { greater_than: 0 }, allow_nil: true
 end
